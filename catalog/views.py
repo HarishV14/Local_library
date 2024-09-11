6d3be5ae7dfde87d20d2d17dadc2a8927a781e93
HarishV14 <harishv@testpress.in>
2024-09-11 19:01:49 +0530

    Commit started BookDetailView

--- catalog/views.py
+++ catalog/views.py
@@ -32,3 +32,26 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='s')[:5] # Get 5 books containing the title war
+    
+    # def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        # context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        # context['some_data'] = 'This is just some data'
+        # return context
+
+from django.shortcuts import get_object_or_404
+
+class BookDetailView(generic.DetailView):
+    model=Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
