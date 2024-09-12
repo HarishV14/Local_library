d1471c6e465aebcdf3ab3068794f4524dad513eb
HarishV14 <harishv@testpress.in>
2024-09-12 18:19:11 +0530

    pending FormConcept yet for model form concept

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('Staffbooks/', views.BookBoorowedByLibrarian.as_view(), name='staff-borrowed'),
+
+    # forms 
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
