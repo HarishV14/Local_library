8e86dfc0d9cff91ff06939582a2bdf81fc17d3d5
HarishV14 <harishv@testpress.in>
2024-09-13 18:43:02 +0530

    Changes1

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -154,7 +154,7 @@ STORAGES = {
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://hamishwillee.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://harih14.pythonanywhere.com']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
